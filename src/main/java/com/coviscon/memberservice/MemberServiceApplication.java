00b52079e8cda8f84dd855dcecb314b228598ef0
Lee-HyunSoo <cris_lx@naver.com>
2023-12-20 03:03:58 +0900

    redis option

--- src/main/java/com/coviscon/memberservice/MemberServiceApplication.java
+++ src/main/java/com/coviscon/memberservice/MemberServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @EnableDiscoveryClient
 @EnableJpaAuditing
@@ -19,5 +20,10 @@ public class MemberServiceApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
 	}
 }
