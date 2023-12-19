ec5e481de31fe11e46afd8ae88306196c442bd79
Lee-HyunSoo <cris_lx@naver.com>
2023-12-20 08:35:10 +0900

    update home controller

--- src/main/java/com/coviscon/memberservice/controller/HomeController.java
+++ src/main/java/com/coviscon/memberservice/controller/HomeController.java
@@ -1,16 +1,32 @@
 package com.coviscon.memberservice.controller;
 
 import com.coviscon.memberservice.dto.MemberResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
-    @GetMapping(value = {"", "/"})
+    private final Environment env;
+
+    @GetMapping(value = "/")
+    public String redirectHome(HttpSession session, Model model) {
+
+        if (session.getAttribute("member") != null) {
+            MemberResponseDto member = (MemberResponseDto) session.getAttribute("member");
+            model.addAttribute("member", member);
+        }
+
+        return env.getProperty("redirect.url") + "home";
+    }
+
+    @GetMapping(value = "/home")
     public String home(HttpSession session, Model model) {
 
         if (session.getAttribute("member") != null) {
