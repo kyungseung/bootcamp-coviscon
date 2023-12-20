327c4f86e25ee293ab7e2c426ac6017636c3a7dd
Lee-HyunSoo <cris_lx@naver.com>
2023-12-20 22:59:04 +0900

    update item

--- src/main/java/com/coviscon/itemservice/service/ItemServiceImpl.java
+++ src/main/java/com/coviscon/itemservice/service/ItemServiceImpl.java
@@ -24,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
@@ -254,10 +255,9 @@ public class ItemServiceImpl implements ItemService {
         if (!multipartFile.isEmpty()) {
             Path filePath = Paths.get(savedImage, multipartFile.getOriginalFilename());
             log.info("[ItemServiceImpl uploadImg] filePath : {}", filePath);
-            try (OutputStream os = Files.newOutputStream(filePath)) {
-                log.info("aaaaaaaaaaaaaaaaaaaaaakk");
+//            try (OutputStream os = Files.newOutputStream(filePath)) {
+            try (OutputStream os = new FileOutputStream(String.valueOf(filePath))) {
                 os.write(multipartFile.getBytes());
-                log.info("asdfadsfadsfadsfsdafdsaf");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
